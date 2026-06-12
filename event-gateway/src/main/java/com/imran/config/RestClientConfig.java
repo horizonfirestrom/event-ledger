aa0ef36c6dce3e8b41b1b618e9df3a80fb82b36f
MD Imran <horizonfirestrom383@gmail.com>
2026-06-11 22:35:00 -0500

    Added Docker Compose support for local deployment

--- event-gateway/src/main/java/com/imran/config/RestClientConfig.java
+++ event-gateway/src/main/java/com/imran/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.imran.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,9 +9,11 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient accountServiceRestClient() {
+    public RestClient accountServiceRestClient(
+            @Value("${account.service.base-url}") String accountServiceBaseUrl) {
+
         return RestClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(accountServiceBaseUrl)
                 .build();
     }
 }
